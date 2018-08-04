63f5a1625b17e57c314cffd1f1c11119cc737ce9
snarksliveshere <snarksliveshere@gmail.com>
2018-08-04 17:06:18 +0300

    initial commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: snarks
+ * Date: 02.08.18
+ * Time: 21:41
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController
+{
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * BlogController constructor.
+     * @param \Twig_Environment $twig
+     * @param SessionInterface $session
+     * @param RouterInterface $router
+     */
+    public function __construct(
+        \Twig_Environment $twig,
+        SessionInterface $session,
+        RouterInterface $router
+    )
+    {
+        $this->twig = $twig;
+        $this->session = $session;
+        $this->router = $router;
+    }
+
+
+    /**
+     * @Route("/", name="blog_index")
+     */
+    public function index()
+    {
+        $html = $this->twig->render(
+            'blog/index.html.twig',
+            [
+            'posts' => $this->session->get('posts')
+            ]
+        );
+
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/add", name="blog_add")
+     */
+    public function add()
+    {
+        $posts = $this->session->get('posts');
+        $posts[uniqid()] = [
+            'title' => 'A random title ' . rand(1, 500),
+            'text' => 'A random text ' . rand(1, 500),
+            'date' => new \DateTime(),
+
+        ];
+
+        $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
+    }
+
+    /**
+     * @Route("/show/{id}", name="blog_show")
+     */
+    public function show($id)
+    {
+        $posts = $this->session->get('posts');
+
+        if (!$posts || !isset($posts[$id])) {
+            throw new NotFoundHttpException('Post not found');
+        }
+
+        $html = $this->twig->render(
+            'blog/post.html.twig',
+            [
+                'id' => $id,
+                'post' => $posts[$id]
+
+
+            ]
+        );
+
+        return new Response($html);
+    }
+}
---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=280)
+     */
+    private $text;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time): void
+    {
+        $this->time = $time;
+    }
+
+}
